f8a31f46a804efadf44c16107798a985173808c2
zoology2007 <90775711+zoology2007@users.noreply.github.com>
2021-09-23 12:32:08 +0530

    Add files via upload

---
+++ comparing number.py
@@ -0,0 +1,9 @@
+print ("welcome to 'comparing number' bot")
+no_1= int(input ("enter first number:")) 
+no_2= int(input ("enter second number:"))
+if no_1>no_2:
+    print (no_1, "is greater than", no_2)
+elif no_1==no_2:
+    print (no_1, "is equal to", no_2)
+else:
+    print (no_1, "is lesser than", no_2)
